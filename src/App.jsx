a6eaebbb95a7d763e18374b98f5899789800c9e7
sedulous11 <seduloussofttech11@gmail.com>
2025-05-01 00:39:15 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ChatBox from "./ChatBox";
+import "./App.css";
+function App() {
+  return (
+    <div className="container">
+      <h2>💬 React ChatBot</h2>
+      <ChatBox />
+    </div>
+  );
+}
+
+export default App;
